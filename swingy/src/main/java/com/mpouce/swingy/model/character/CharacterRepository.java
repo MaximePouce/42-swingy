740993dd818187e9b0132ddb53dfcb477a3c19e5
Scelewyn <Scelewyn@gmail.com>
2024-07-02 18:20:09 +0200

    WIP Function to get all characters in the Database

--- swingy/src/main/java/com/mpouce/swingy/model/character/CharacterRepository.java
+++ swingy/src/main/java/com/mpouce/swingy/model/character/CharacterRepository.java
@@ -1,10 +1,12 @@
 package com.mpouce.swingy.model.character;
 
 import com.mpouce.swingy.model.utils.DatabaseUtils;
+import com.mpouce.swingy.model.utils.DatabaseConnection;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.ResultSet;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +15,20 @@ public class CharacterRepository {
 
     public CharacterRepository() {
 
+    }
+
+    public List<Character> getCharacters() {
+        List<Character> characters = new ArrayList<>();
+        try {
+            Statement st = DatabaseConnection.getInstance().getConnection().createStatement();
+            ResultSet rs = st.executeQuery(
+                            "SELECT characters.characterid, characters.name, characters.experience, characters.classid "
+                            + "FROM characters INNER JOIN classes ON characters.classid = classes.classid;");
+            DatabaseUtils.printResultSet(rs);
+        } catch (SQLException e) {
+            System.out.println("Unable to connect to Database: " + e.getMessage());
+        }
+        return characters;
     }
 
     public void getClasses() {
