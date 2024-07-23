a357c7f18494f892cb6eb82e8b4aa1e50a632547
Scelewyn <Scelewyn@gmail.com>
2024-07-23 03:20:35 +0200

    Using columns to center content

--- swingy/src/main/java/com/mpouce/swingy/view/Window.java
+++ swingy/src/main/java/com/mpouce/swingy/view/Window.java
@@ -51,18 +51,48 @@ public class Window {
         });
     }
 
+    public void addSideColumns() {
+        SwingUtilities.invokeLater(() -> {
+            JPanel rightPanel = new JPanel();
+            rightPanel.setOpaque(false);
+
+            GridBagConstraints c = new GridBagConstraints();
+
+            c.gridx = 0;
+            c.gridy = 0;
+            c.weightx = 0.1;
+            c.weighty = 1.0;
+            c.anchor = GridBagConstraints.LINE_START;
+            c.fill = GridBagConstraints.BOTH;
+
+            this.view.add(rightPanel, c);
+
+            JPanel leftPanel = new JPanel();
+            leftPanel.setOpaque(false);
+
+            c.gridx = 2;
+            c.gridy = 0;
+            c.weightx = 0.1;
+            c.weighty = 1.0;
+            c.anchor = GridBagConstraints.LINE_END;
+            c.fill = GridBagConstraints.BOTH;
+
+            this.view.add(leftPanel, c);
+        });
+    }
+
     public void addMenu() {
         SwingUtilities.invokeLater(() -> {
             JPanel menuPanel = new JPanel();
             JLabel lblTest = new JLabel("Test");
-            menuPanel.setBackground(Color.magenta);
+            menuPanel.setOpaque(false);
             menuPanel.add(lblTest);
 
             GridBagConstraints c = new GridBagConstraints();
 
             c.gridx = 0;
             c.gridy = 0;
-            c.weightx = 0.4;
+            c.weightx = 0.1;
             c.weighty = 1.0;
             c.anchor = GridBagConstraints.LINE_START;
             c.fill = GridBagConstraints.BOTH;
@@ -77,9 +107,9 @@ public class Window {
 
             c.gridx = 1;
             c.gridy = 0;
-            c.weightx = 0.6;
-            c.weighty = 1.0;
-            c.anchor = GridBagConstraints.LINE_END;
+            c.weightx = 0.8;
+            c.weighty = 1;
+            c.anchor = GridBagConstraints.CENTER;
             c.fill = GridBagConstraints.BOTH;
 
             this.view.add(contentPanel, c);
