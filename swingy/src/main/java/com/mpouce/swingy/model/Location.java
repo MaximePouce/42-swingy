dc8173efe4c3ac2229ea8abbfd5700211a8a7764
Scelewyn <Scelewyn@gmail.com>
2024-07-29 06:09:23 +0200

    WIP Individual location class

---
+++ swingy/src/main/java/com/mpouce/swingy/model/Location.java
@@ -0,0 +1,21 @@
+package com.mpouce.swingy.model;
+
+import com.mpouce.swingy.model.character.Character;
+
+public class Location {
+    private int x;
+    private int y;
+    private Character character;
+
+    public Location(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.character = null;
+    }
+
+    public Location(int x, int y, Character character) {
+        this.x = x;
+        this.y = y;
+        this.character = character;
+    }
+}
