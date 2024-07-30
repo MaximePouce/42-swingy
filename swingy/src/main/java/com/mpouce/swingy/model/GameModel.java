01b6f8a30f55a3c756302a0cec7c6fbbdf27fae2
Scelewyn <Scelewyn@gmail.com>
2024-07-30 04:19:26 +0200

    Setting player location on map generation

--- swingy/src/main/java/com/mpouce/swingy/model/GameModel.java
+++ swingy/src/main/java/com/mpouce/swingy/model/GameModel.java
@@ -1,12 +1,14 @@
 package com.mpouce.swingy.model;
 
+import com.mpouce.swingy.model.character.Character;
+
 public class GameModel {
     public GameModel() {}
 
-    public Location[][] getMap(int playerId, int playerLevel) {
+    public Location[][] getMap(Character player) {
         // Look for map saved
         // If not found, start a new one
-        int mapSize = (playerLevel - 1) * 5 + 10;
+        int mapSize = (player.getLevel() - 1) * 5 + 10;
         System.out.println("Creating new map with size " + mapSize);
         Location[][] map = new Location[mapSize][mapSize];
 
@@ -16,6 +18,7 @@ public class GameModel {
                 map[x][y] = new Location(x, y);
             }
         }
+        player.setLocation(map[mapSize / 2][mapSize / 2]);
 
         return map;
     }
