620447d2b52144cd99834cafc74941c0b65dcb0c
Scelewyn <Scelewyn@gmail.com>
2024-07-31 02:52:52 +0200

    Location with SQL creation

--- swingy/src/main/java/com/mpouce/swingy/model/Location.java
+++ swingy/src/main/java/com/mpouce/swingy/model/Location.java
@@ -1,15 +1,24 @@
 package com.mpouce.swingy.model;
 
 import com.mpouce.swingy.model.character.Character;
+import com.mpouce.swingy.model.utils.DatabaseConnection;
+import com.mpouce.swingy.model.utils.DatabaseUtils;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 
 public class Location {
+    private int locationId;
     private int x;
     private int y;
     private Character character;
+    private int mapId;
 
-    public Location(int x, int y) {
+    public Location(int x, int y, int mapId) {
         this.x = x;
         this.y = y;
+        this.mapId = mapId;
         this.character = null;
     }
 
@@ -39,5 +48,29 @@ public class Location {
 
     public boolean isFinish(int mapSize) {
         return (this.x == 0 || this.x == mapSize - 1 || this.y == 0 || this.y == mapSize - 1);
+    }
+
+    public void createLocation() {
+        int newId = -1;
+        String prepStatement = "INSERT INTO locations (x, y, mapId) VALUES (?, ?, ?)";
+        try {
+            PreparedStatement st = DatabaseConnection.getInstance().getConnection()
+                                    .prepareStatement(prepStatement, Statement.RETURN_GENERATED_KEYS);
+            st.setInt(1, this.x);
+            st.setInt(2, this.y);
+            st.setInt(3, this.mapId);
+            int affectedRows = st.executeUpdate();
+            if (affectedRows == 0) {
+                throw new SQLException("Creation failed. No rows affected.");
+            }
+            ResultSet rs = st.getGeneratedKeys();
+            if (rs.next()) {
+                newId = rs.getInt(1);
+            }
+            rs.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        this.locationId = newId;
     }
 }
